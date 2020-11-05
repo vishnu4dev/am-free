35387612d943fbdae8a408f051d7a5aa7ffdd0f1
unknown <vishnuashokan5@gmail.com>
2020-11-05 22:57:09 +0530

    login page implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { 
-	BrowserRouter as Router, 
-	Route, 
-	Switch 
-} from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Explore from "./Container/Explore";
 import LoginPage from "./Container/LoginPage";
+import AddNewPost from "./Container/AddNewPost";
 
 function App() {
   return (
     <Router>
-      <Switch >
+      <Switch>
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/explore" component={Explore} />
+        <Route exact path="/addNewPost" component={AddNewPost} />
       </Switch>
     </Router>
   );
